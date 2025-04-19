acc26afbd06b3c68f0ec8e6028a543f76104923b
Oleg <famouse2016@gmail.com>
2025-04-19 14:39:13 +0300

    lab1

--- lab1/mainapp/views.py
+++ lab1/mainapp/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render
+
+
+from django.conf import settings
+
+
+import rest_framework
+from rest_framework import  viewsets
+
+
+from .models import Employee
+from .serializers import EmployeeSerializer
+
+
+class EmployeeViewSet(viewsets.ModelViewSet):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
